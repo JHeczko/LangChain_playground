f754ba408719b6f0dd41dc4470ec1dc35a31adba
JHeczko <jakub.heczko5@gmail.com>
2025-08-21 16:20:17 +0200

    backend in flask added and simple frontend

--- person_summary/ice_braker.py
+++ person_summary/ice_braker.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -5,11 +7,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from person_summary.scraping_tools.linkedln import scrape_linkedin
 from agents.linked_lookup import lookup_linkedin
 
-from output_parsers.output_parser import summary_parser
+from output_parsers.output_parser import summary_parser, Summary
 
-def ice_break_with(name: str):
+def ice_break_with(name: str, mock: bool = True) -> Tuple[Summary, str]:
     linkedin_url = lookup_linkedin(name=name)
-    linkedin_data = scrape_linkedin(linkedin_url, mock=True)
+    linkedin_data = scrape_linkedin(linkedin_url, mock=mock)
 
     messege_template = """
             Given the information {info} about a person from linkedin i want you to create:
@@ -26,14 +28,13 @@ def ice_break_with(name: str):
 
     llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-2.0-flash")
 
-    chain = prompt_template | llm
+    chain = prompt_template | llm | summary_parser
 
     res = chain.invoke(input={
         "info": linkedin_data
     })
 
-    print(res.content)
-
+    return res, linkedin_data['photoUrl']
 
 if __name__ == "__main__":
 
